bb092d08623e493c82a848b65fb3cfc45eb17c54
klfischer89 <fischer-k4@web.de>
2026-01-10 09:16:39 +0100

    added maxpoolong2d layers to make training morge efficeint cause the model has to train less params and balance overfitting

--- cnn.py
+++ cnn.py
@@ -55,7 +55,9 @@ y_train = to_categorical(y_train)
 model = keras.Sequential([
     keras.Input(shape = (28, 28, 1)),
     layers.Conv2D(10, kernel_size = (3, 3), padding = "same", activation = "relu"),
+    layers.MaxPooling2D(),
     layers.Conv2D(10, kernel_size = (3, 3), padding = "same", activation = "relu"),
+    layers.MaxPooling2D(),
     layers.Flatten(),
     layers.Dense(100, activation = "relu"),
     layers.Dense(10, activation = "softmax")
